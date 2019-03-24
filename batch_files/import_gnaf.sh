187d1edd1df93f0fa33bdca95c77adec1160de5a
gisisfun <45181329+gisisfun@users.noreply.github.com>
2019-03-24 19:20:29 +1100

    Create import_gnaf.sh

---
+++ batch_files/import_gnaf.sh
@@ -0,0 +1,153 @@
+
+sh dbcsvfun.sh NSW_ADDRESS_ALIAS_psv db
+sh dbcsvfun.sh ACT_ADDRESS_ALIAS_psv db
+sh dbcsvfun.sh VIC_ADDRESS_ALIAS_psv db
+sh dbcsvfun.sh QLD_ADDRESS_ALIAS_psv db
+sh dbcsvfun.sh SA_ADDRESS_ALIAS_psv db
+sh dbcsvfun.sh NT_ADDRESS_ALIAS_psv db
+sh dbcsvfun.sh WA_ADDRESS_ALIAS_psv db
+sh dbcsvfun.sh TAS_ADDRESS_ALIAS_psv db
+
+sh dbcsvfun.sh NSW_ADDRESS_DEFAULT_GEOCODE_psv db
+sh dbcsvfun.sh ACT_ADDRESS_DEFAULT_GEOCODE_psv db
+sh dbcsvfun.sh VIC_ADDRESS_DEFAULT_GEOCODE_psv db
+sh dbcsvfun.sh QLD_ADDRESS_DEFAULT_GEOCODE_psv db
+sh dbcsvfun.sh SA_ADDRESS_DEFAULT_GEOCODE_psv db
+sh dbcsvfun.sh NT_ADDRESS_DEFAULT_GEOCODE_psv db
+sh dbcsvfun.sh WA_ADDRESS_DEFAULT_GEOCODE_psv db
+sh dbcsvfun.sh TAS_ADDRESS_DEFAULT_GEOCODE_psv db
+
+sh dbcsvfun.sh NSW_ADDRESS_DETAIL_psv db
+sh dbcsvfun.sh ACT_ADDRESS_DETAIL_psv db
+sh dbcsvfun.sh VIC_ADDRESS_DETAIL_psv db
+sh dbcsvfun.sh QLD_ADDRESS_DETAIL_psv db
+sh dbcsvfun.sh SA_ADDRESS_DETAIL_psv db
+sh dbcsvfun.sh NT_ADDRESS_DETAIL_psv db
+sh dbcsvfun.sh WA_ADDRESS_DETAIL_psv db
+sh dbcsvfun.sh TAS_ADDRESS_DETAIL_psv db
+
+sh dbcsvfun.sh NSW_ADDRESS_FEATURE_psv db
+sh dbcsvfun.sh ACT_ADDRESS_FEATURE_psv db
+sh dbcsvfun.sh VIC_ADDRESS_FEATURE_psv db
+sh dbcsvfun.sh QLD_ADDRESS_FEATURE_psv db
+sh dbcsvfun.sh SA_ADDRESS_FEATURE_psv db
+sh dbcsvfun.sh NT_ADDRESS_FEATURE_psv db
+sh dbcsvfun.sh WA_ADDRESS_FEATURE_psv db
+sh dbcsvfun.sh TAS_ADDRESS_FEATURE_psv db
+
+sh dbcsvfun.sh NSW_ADDRESS_GEOCODE_psv db
+sh dbcsvfun.sh ACT_ADDRESS_GEOCODE_psv db
+sh dbcsvfun.sh VIC_ADDRESS_GEOCODE_psv db
+sh dbcsvfun.sh QLD_ADDRESS_GEOCODE_psv db
+sh dbcsvfun.sh SA_ADDRESS_GEOCODE_psv db
+sh dbcsvfun.sh NT_ADDRESS_GEOCODE_psv db
+sh dbcsvfun.sh WA_ADDRESS_GEOCODE_psv db
+sh dbcsvfun.sh TAS_ADDRESS_GEOCODE_psv db
+
+sh dbcsvfun.sh NSW_ADDRESS_MESH_BLOCK_2011_psv db
+sh dbcsvfun.sh ACT_ADDRESS_MESH_BLOCK_2011_psv db
+sh dbcsvfun.sh VIC_ADDRESS_MESH_BLOCK_2011_psv db
+sh dbcsvfun.sh QLD_ADDRESS_MESH_BLOCK_2011_psv db
+sh dbcsvfun.sh SA_ADDRESS_MESH_BLOCK_2011_psv db
+sh dbcsvfun.sh NT_ADDRESS_MESH_BLOCK_2011_psv db
+sh dbcsvfun.sh WA_ADDRESS_MESH_BLOCK_2011_psv db
+sh dbcsvfun.sh TAS_ADDRESS_MESH_BLOCK_2011_psv db
+
+sh dbcsvfun.sh NSW_ADDRESS_MESH_BLOCK_2016_psv db
+sh dbcsvfun.sh ACT_ADDRESS_MESH_BLOCK_2016_psv db
+sh dbcsvfun.sh VIC_ADDRESS_MESH_BLOCK_2016_psv db
+sh dbcsvfun.sh QLD_ADDRESS_MESH_BLOCK_2016_psv db
+sh dbcsvfun.sh SA_ADDRESS_MESH_BLOCK_2016_psv db
+sh dbcsvfun.sh NT_ADDRESS_MESH_BLOCK_2016_psv db
+sh dbcsvfun.sh WA_ADDRESS_MESH_BLOCK_2016_psv db
+sh dbcsvfun.sh TAS_ADDRESS_MESH_BLOCK_2016_psv db
+
+sh dbcsvfun.sh NSW_ADDRESS_SITE_GEOCODE_psv db
+sh dbcsvfun.sh ACT_ADDRESS_SITE_GEOCODE_psv db
+sh dbcsvfun.sh VIC_ADDRESS_SITE_GEOCODE_psv db
+sh dbcsvfun.sh QLD_ADDRESS_SITE_GEOCODE_psv db
+sh dbcsvfun.sh SA_ADDRESS_SITE_GEOCODE_psv db
+sh dbcsvfun.sh NT_ADDRESS_SITE_GEOCODE_psv db
+sh dbcsvfun.sh WA_ADDRESS_SITE_GEOCODE_psv db
+sh dbcsvfun.sh TAS_ADDRESS_SITE_GEOCODE_psv db
+
+sh dbcsvfun.sh NSW_ADDRESS_SITE_psv db
+sh dbcsvfun.sh ACT_ADDRESS_SITE_psv db
+sh dbcsvfun.sh VIC_ADDRESS_SITE_psv db
+sh dbcsvfun.sh QLD_ADDRESS_SITE_psv db
+sh dbcsvfun.sh SA_ADDRESS_SITE_psv db
+sh dbcsvfun.sh NT_ADDRESS_SITE_psv db
+sh dbcsvfun.sh WA_ADDRESS_SITE_psv db
+sh dbcsvfun.sh TAS_ADDRESS_SITE_psv db
+
+sh dbcsvfun.sh NSW_LOCALITY_ALIAS_psv db
+sh dbcsvfun.sh ACT_LOCALITY_ALIAS_psv db
+sh dbcsvfun.sh VIC_LOCALITY_ALIAS_psv db
+sh dbcsvfun.sh QLD_LOCALITY_ALIAS_psv db
+sh dbcsvfun.sh SA_LOCALITY_ALIAS_psv db
+sh dbcsvfun.sh NT_LOCALITY_ALIAS_psv db
+sh dbcsvfun.sh WA_LOCALITY_ALIAS_psv db
+sh dbcsvfun.sh TAS_LOCALITY_ALIAS_psv db
+
+sh dbcsvfun.sh NSW_LOCALITY_NEIGHBOUR_psv db
+sh dbcsvfun.sh ACT_LOCALITY_NEIGHBOUR_psv db
+sh dbcsvfun.sh VIC_LOCALITY_NEIGHBOUR_psv db
+sh dbcsvfun.sh QLD_LOCALITY_NEIGHBOUR_psv db
+sh dbcsvfun.sh SA_LOCALITY_NEIGHBOUR_psv db
+sh dbcsvfun.sh NT_LOCALITY_NEIGHBOUR_psv db
+sh dbcsvfun.sh WA_LOCALITY_NEIGHBOUR_psv db
+sh dbcsvfun.sh TAS_LOCALITY_NEIGHBOUR_psv db
+
+sh dbcsvfun.sh NSW_LOCALITY_POINT_psv db
+sh dbcsvfun.sh ACT_LOCALITY_POINT_psv db
+sh dbcsvfun.sh VIC_LOCALITY_POINT_psv db
+sh dbcsvfun.sh QLD_LOCALITY_POINT_psv db
+sh dbcsvfun.sh SA_LOCALITY_POINT_psv db
+sh dbcsvfun.sh NT_LOCALITY_POINT_psv db
+sh dbcsvfun.sh WA_LOCALITY_POINT_psv db
+sh dbcsvfun.sh TAS_LOCALITY_POINT_psv db
+
+sh dbcsvfun.sh NSW_LOCALITY_psv db
+sh dbcsvfun.sh ACT_LOCALITY_psv db
+sh dbcsvfun.sh VIC_LOCALITY_psv db
+sh dbcsvfun.sh QLD_LOCALITY_psv db
+sh dbcsvfun.sh SA_LOCALITY_psv db
+sh dbcsvfun.sh NT_LOCALITY_psv db
+sh dbcsvfun.sh WA_LOCALITY_psv db
+sh dbcsvfun.sh TAS_LOCALITY_psv db
+
+sh dbcsvfun.sh NSW_MB_2011_psv db
+sh dbcsvfun.sh ACT_MB_2011_psv db
+sh dbcsvfun.sh VIC_MB_2011_psv db
+sh dbcsvfun.sh QLD_MB_2011_psv db
+sh dbcsvfun.sh SA_MB_2011_psv db
+sh dbcsvfun.sh NT_MB_2011_psv db
+sh dbcsvfun.sh WA_MB_2011_psv db
+sh dbcsvfun.sh TAS_MB_2011_psv db
+
+sh dbcsvfun.sh NSW_MB_2016_psv db
+sh dbcsvfun.sh ACT_MB_2016_psv db
+sh dbcsvfun.sh VIC_MB_2016_psv db
+sh dbcsvfun.sh QLD_MB_2016_psv db
+sh dbcsvfun.sh SA_MB_2016_psv db
+sh dbcsvfun.sh NT_MB_2016_psv db
+sh dbcsvfun.sh WA_MB_2016_psv db
+sh dbcsvfun.sh TAS_MB_2016_psv db
+
+sh dbcsvfun.sh NSW_PRIMARY_SECONDARY_psv db
+sh dbcsvfun.sh ACT_PRIMARY_SECONDARY_psv db
+sh dbcsvfun.sh VIC_PRIMARY_SECONDARY_psv db
+sh dbcsvfun.sh QLD_PRIMARY_SECONDARY_psv db
+sh dbcsvfun.sh SA_PRIMARY_SECONDARY_psv db
+sh dbcsvfun.sh NT_PRIMARY_SECONDARY_psv db
+sh dbcsvfun.sh WA_PRIMARY_SECONDARY_psv db
+sh dbcsvfun.sh TAS_PRIMARY_SECONDARY_psv db
+
+sh dbcsvfun.sh NSW_STATE_psv db
+sh dbcsvfun.sh ACT_STATE_psv db
+sh dbcsvfun.sh VIC_STATE_psv db
+sh dbcsvfun.sh QLD_STATE_psv db
+sh dbcsvfun.sh SA_STATE_psv db
+sh dbcsvfun.sh NT_STATE_psv db
+sh dbcsvfun.sh WA_STATE_psv db
+sh dbcsvfun.sh TAS_STATE_psv db
