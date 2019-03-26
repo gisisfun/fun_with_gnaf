bf5e710ef4877efb6a8365b2131185c8113792f3
gisisfun <45181329+gisisfun@users.noreply.github.com>
2019-03-27 09:10:00 +1100

    Update address_view.sql

--- spatialite_db/address_view.sql
+++ spatialite_db/address_view.sql
@@ -3,66 +3,65 @@ CREATE VIEW ADDRESS_VIEW
 AS
 
 SELECT
-(BUILDING_NAME || " " || 
- NUMBER_FIRST || " " || 
- STREET_NAME || " " || 
- LOCALITY_NAME || " " ||
- STATE_ABBREVIATION || " " || 
- POSTCODE  ) as AddressText
-AD.address_detail_pid as ADDRESS_DETAIL_PID,
-AD.street_locality_pid as STREET_LOCALITY_PID,
-AD.locality_pid as LOCALITY_PID,
-AD.building_name as BUILDING_NAME,
+(Building_Name || " " || 
+ Number_First || " " || 
+ Street_Name || " " || 
+ Locality_Name || " " ||
+ State_Abbreviation || " " || 
+ Postcode  ) as AddressText
+AD.address_detail_pid as Address_Detail_PID,
+AD.street_locality_pid as Street_Locality_PID,
+AD.locality_pid as Locality_PID,
+AD.building_name as Building_Name,
 
-AD.lot_number_prefix as LOT_NUMBER_PREFIX,
-AD.lot_number as LOT_NUMBER,
-AD.lot_number_suffix as LOT_NUMBER_SUFFIX,
+AD.lot_number_prefix as Lot_Number_Prefix,
+AD.lot_number as Lot_Number,
+AD.lot_number_suffix as Lot_Number_Suffix,
 
-FTA.name as FLAT_TYPE,
-AD.flat_number_prefix as FLAT_NUMBER_PREFIX,
-AD.flat_number as FLAT_NUMBER,
-AD.flat_number_suffix as FLAT_NUMBER_SUFFIX,
+FTA.name as Flat_Type,
+AD.flat_number_prefix as Flat_Number_Prefix,
+AD.flat_number as Flat_Number,
+AD.flat_number_suffix as Flat_Number_Suffix,
 
-LTA.name as LEVEL_TYPE,
-AD.level_number_prefix as LEVEL_NUMBER_PREFIX,
-AD.level_number as LEVEL_NUMBER,
-AD.level_number_suffix as LEVEL_NUMBER_SUFFIX,
+LTA.name as Level_Type,
+AD.level_number_prefix as Level_Number_Prefix,
+AD.level_number as Level_Number,
+AD.level_number_suffix as Level_Number_Suffix,
 
-AD.number_first_prefix as NUMBER_FIRST_PREFIX,
-AD.number_first as NUMBER_FIRST,
-AD.number_first_suffix as NUMBER_FIRST_SUFFIX,
-AD.number_last_prefix as NUMBER_LAST_PREFIX,
-AD.number_last as NUMBER_LAST,
-AD.number_last_suffix as NUMBER_LAST_SUFFIX,
+AD.number_first_prefix as Number_First_Prefix,
+AD.number_first as Number_First,
+AD.number_first_suffix as Number_First_Suffix,
+AD.number_last_prefix as Number_Last_Prefix,
+AD.number_last as Number_Last,
+AD.number_last_suffix as Number_Last_Suffix,
 
-SL.street_name as STREET_NAME,
-SL.street_class_code as STREET_CLASS_CODE,
-SCA.name as STREET_CLASS_TYPE,
-SL.street_type_code as STREET_TYPE_CODE,
-SL.street_suffix_code as STREET_SUFFIX_CODE,
-SSA.name as STREET_SUFFIX_TYPE,
+SL.street_name as Street_Name,
+SL.street_class_code as Street_Class_Code,
+SCA.name as Street_Class_Type,
+SL.street_type_code as Street_Type_Code,
+SL.street_suffix_code as Street_Suffix_Code,
+SSA.name as STreet_Suffix_Type,
 
-L.locality_name as LOCALITY_NAME,
+L.locality_name as Locality_Name,
 
-ST.state_abbreviation as STATE_ABBREVIATION,
+ST.state_abbreviation as State_Abbreviation,
 
-AD.postcode as POSTCODE,
+AD.postcode as Postcode,
 
-ADG.latitude as LATITUDE,
-ADG.longitude as LONGITUDE,
-GTA.name as GEOCODE_TYPE,
+ADG.latitude as Latitude,
+ADG.longitude as Longitude,
+GTA.name as Geocode_Type,
 
-AD.confidence as CONFIDENCE,
+AD.confidence as Confidence,
 
-AD.alias_principal as ALIAS_PRINCIPAL,
-AD.primary_secondary as PRIMARY_SECONDARY,
+AD.alias_principal as Alias_Principal,
+AD.primary_secondary as Primary_Secondary,
 
-AD.legal_parcel_id as LEGAL_PARCEL_ID,
+AD.legal_parcel_id as Legal_Parcel_ID,
 
-AD.date_created as DATE_CREATED
+AD.date_created as Date_Created
 
 FROM
-
 [ADDRESS_DETAIL] as AD 
 LEFT JOIN [FLAT_TYPE_AUT] as FTA ON AD.flat_type_code=FTA.code
 LEFT JOIN [LEVEL_TYPE_AUT] as LTA ON AD.level_type_code=LTA.code
