00d3735a23317f4cff41cd06f8b9e71b73d5c4fd
Damien Clarke <45181329+gisisfun@users.noreply.github.com>
2019-11-04 13:25:27 +1100

    Delete geocode.ps1

--- powershell/geocode.ps1
+++
@@ -1,2 +0,0 @@
-param([string]$myaddrresses,[string]$gnaf,[string]$processed);
-echo "" | Out-File -FilePath Process.txt;foreach($line in [System.IO.File]::ReadLines("myaddresses.txt")){ echo $line| Out-File -FilePath Process.txt -Append; get-content res_street_locality.csv | select-string $line| select -first 1 | Out-File -FilePath Process.txt -Append}
