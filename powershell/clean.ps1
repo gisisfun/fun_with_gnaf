3b8138f8b0d4711038df571396891480dabd1dac
Damien Clarke <45181329+gisisfun@users.noreply.github.com>
2019-11-04 13:25:14 +1100

    Delete clean.ps1

--- powershell/clean.ps1
+++
@@ -1,2 +0,0 @@
-param([string]$rawaddrresses,[string]$cleanaddreses);
-get-content rawaddresses.txt | % {$_.replace("/"," ")} |% {$_.replace("cres ","crescent ")} | % {$_.replace(","," ")} |% {$_.replace("st ","street ")} |% {$_.replace("  " , " ")} | out-file myaddresses.txt
