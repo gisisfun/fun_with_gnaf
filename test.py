7d6b2d998fc388862610014115ff4c55640a5449
Damien Clarke <45181329+gisisfun@users.noreply.github.com>
2019-11-06 08:25:13 +1100

    Add files via upload

--- test.py
+++ test.py
@@ -22,7 +22,7 @@ from gnaf.thecode import Process
 #c.drop_states_sql_st(p)
 gnafP = 'AUG19_GNAF_PipeSeparatedValue/G-NAF_AUGUST_2019/'
 ds = Process.Do_Stuff()
-ds.sql_to_db(Process.Collate.create_merge_standard_sql_st(Tables.Standard.ADDRESS_ALIAS(),gnafP,'Standard/'),'testing')
+ds.sql_to_db(Process.Collate.create_merge_standard_sql_st(Tables.Standard.ADDRESS_DEFAULT_GEOCODE(),gnafP,'Standard/'),'testing')
 
 #Process.Collate.create_merge_auth_sql_st(Tables.Authority_Code.ADDRESS_ALIAS_TYPE_AUT(),gnafP,'Authority_Code/')
 
