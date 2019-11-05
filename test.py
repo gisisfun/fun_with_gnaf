32a6829ac983ce9fdb04e72802996277339c1bc7
Damien Clarke <45181329+gisisfun@users.noreply.github.com>
2019-11-05 16:36:25 +1100

    Add files via upload

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from gnaf.parameters import Tables,Defaults
 from gnaf.thecode import Process
-d = Defaults()
+#d = Defaults()
 
 #upd = 'UPDATE'
 #
@@ -15,9 +15,14 @@ d = Defaults()
 #    print(thestate)
 #    c.pipes_to_comma(p.filePiped.format(state = thestate))
 #c.state_pipes(p)
-p = Tables.Standard.ADDRESS_ALIAS()
-c = Process.Collect('AUG19_GNAF_PipeSeparatedValue/G-NAF_AUGUST_2019/','Standard/')
+
+#c = Process.Collect('AUG19_GNAF_PipeSeparatedValue/G-NAF_AUGUST_2019/','Standard/')
 #c.drop_states_sql_st(p)
 #c.load_states_sql_st(p)
-Process.Collate.create_merge_state_sql_st(p)
 #c.drop_states_sql_st(p)
+gnafP = 'AUG19_GNAF_PipeSeparatedValue/G-NAF_AUGUST_2019/' 
+#Process.Collate.create_merge_standard_sql_st(Tables.Standard.ADDRESS_ALIAS(),gnafP,'Standard/')
+
+#Process.Collate.create_merge_auth_sql_st(Tables.Authority_Code.ADDRESS_ALIAS_TYPE_AUT(),gnafP,'Authority_Code/')
+
+Process.Collate.create_merge_auth_sql_st(Tables.Authority_Code.ADDRESS_CHANGE_TYPE_AUT(),gnafP,'Authority_Code/')
