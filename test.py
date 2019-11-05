49cd5cae9a789d572982078cecd29dcf33ca660b
Damien Clarke <45181329+gisisfun@users.noreply.github.com>
2019-11-05 11:37:35 +1100

    Add files via upload

--- test.py
+++ test.py
@@ -11,12 +11,13 @@ d = Defaults()
 #        print(upd)
 #    else:
 #        print(';')
+#for thestate in p.stateList:
+#    print(thestate)
+#    c.pipes_to_comma(p.filePiped.format(state = thestate))
+#c.state_pipes(p)
 p = Tables.Standard.ADDRESS_ALIAS()
 c = Process.Collect('AUG19_GNAF_PipeSeparatedValue/G-NAF_AUGUST_2019/','Standard/')
-for thestate in d.states8:
-    print(thestate)
-    c.pipes_to_comma(p.filePiped.format(state = thestate))
-
-
-#
-Process.Collate.create_sql(d.state8,p)
+c.drop_states_sql_st(p)
+c.load_states_sql_st(p)
+Process.Collate.create_merge_sql_st(p.stateList,p)
+c.drop_states_sql_st(p)
